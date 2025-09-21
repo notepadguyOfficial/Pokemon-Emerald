5c1e1a42be9cf93fa15d78749b8fd5092e3b8eff
notepadguyofficial <rerollhashtagnumber1@gmail.com>
2025-09-21 17:30:01 +0000

    Add a EV IV Stat Editor UI

--- src/strings.c
+++ src/strings.c
@@ -284,6 +284,7 @@ const u8 gText_ItemsPocket[] = _("ITEMS");
 const u8 gText_PokeBallsPocket[] = _("POKé BALLS");
 const u8 gText_TMHMPocket[] = _("TMs & HMs");
 const u8 gText_BerriesPocket[] = _("BERRIES");
+const u8 gText_StatEditor[] = _("EDIT STATS");
 const u8 gText_KeyItemsPocket[] = _("KEY ITEMS");
 
 const u8 *const gPocketNamesStringsTable[] =
